9e1de94e2259bb81ae129014c6bc128c281f663f
chandan kumar <106377969+ApexSpunk@users.noreply.github.com>
2023-01-30 16:16:58 +0530

    Update connect.js

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const connect = () => {
-    return 0
+    return mongoose.connect("mongodb+srv://ApexSpunk:masai6X@masai.21uwirt.mongodb.net/mock12?retryWrites=true&w=majority");
 };
 
 module.exports = connect;
